693d2d30aee2cccb0d6d885de1edd63e646c097b
Audun Leganger <audun.leganger@gmail.com>
2025-02-21 19:26:24 +0000

    Refactor song fetching to useSongs hook

--- hexus-site/src/App.tsx
+++ hexus-site/src/App.tsx
@@ -3,77 +3,27 @@ import { Link } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { DarkModeButton } from "./components/DarkModeButton";
+import useSongs from "./hooks/useSongs";
 import { useDarkMode } from "./context/DarkModeContext";
-
-interface Song {
-    id: string;
-    title: string;
-    author: string;
-    melody: string;
-    event: string;
-    year: number;
-    lyrics: string[];
-    comments: string[];
-    start_page: number;
-    end_page: number;
-    mp3: string;
-}
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [titleSearch, setTitleSearch] = useState(true);
     const [textSearch, setTextSearch] = useState(false);
-    const [songs, setSongs] = useState<Song[]>([]);
-    const [loading, setLoading] = useState(true);
     const [currentSort, setCurrentSort] = useState("title asc");
     const { darkMode } = useDarkMode();
+    const { songs, setSongs, loading } = useSongs();
 
     // Load song objects from each song files. Path: ../public/songs/{songId}.json
     useEffect(() => {
         document.title = "Hexus";
-        const fetchSongs = async () => {
-            // Try to fetch songs from localstorage first
-            try {
-                const cachedSongs = localStorage.getItem("songs");
-                let songObjects: Song[];
-                if (cachedSongs) {
-                    songObjects = JSON.parse(cachedSongs);
-                } else {
-                    // Fetch song ids from songs-metadata.json
-                    const response = await fetch("/songs-metadata.json");
-                    const songIds = await response.json();
 
-                    // Load song objects from each line in songIds
-                    songObjects = await Promise.all(
-                        songIds.map(async (songId: string) => {
-                            const response = await fetch(
-                                `/songs/${songId}.json`
-                            );
-                            const songObject = await response.json();
-                            return songObject;
-                        })
-                    );
-                }
-                songObjects.sort((a, b) =>
-                    a.title.localeCompare(b.title, "nb")
-                );
-                console.log(songObjects[0].title);
-                localStorage.setItem("songs", JSON.stringify(songObjects));
-                setSongs(songObjects);
-                setLoading(false);
-            } catch (error) {
-                console.log("Error fetching song objects:", error);
-            }
-        };
-        fetchSongs();
-
-        // Add event listener to clear local storage before unloading page
         const handleBeforeUnload = () => {
             localStorage.removeItem("songs");
         };
+
         window.addEventListener("beforeunload", handleBeforeUnload);
 
-        // Cleanup event listener on component unmount
         return () => {
             window.removeEventListener("beforeunload", handleBeforeUnload);
         };
@@ -149,10 +99,6 @@ const App = () => {
         );
     });
 
-    // const sortedSongs = filteredSongs.sort((a, b) => a.title.localeCompare(b.title));
-    // console.log(sortedSongs[0].title);
-    // console.log(sortedSongs[1].title);
-
     return (
         <div className="centered">
             <h1>Hexus</h1>
@@ -175,54 +121,42 @@ const App = () => {
                 />
                 <label htmlFor="lyricsCheckbox">Tekstsøk</label>
             </section>
-            {
-                loading ? (
-                    <p>Loading...</p>
-                ) : filteredSongs.length > 0 ? (
-                    <section className="centered">
-                        <table className={`${darkMode}`}>
-                            <thead>
-                                <tr>
-                                    <th onClick={handleSortTitleToggle}>
-                                        Tittel
-                                    </th>
-                                    <th onClick={handleSortYearToggle}>År</th>
-                                    <th onClick={handleSortPageToggle}>Side</th>
-                                    <th>Forfatter</th>
-                                    <th>Melodi</th>
-                                    <th>Arrangement</th>
+            {loading ? (
+                <p>Loading...</p>
+            ) : filteredSongs.length > 0 ? (
+                <section className="centered">
+                    <table className={`${darkMode}`}>
+                        <thead>
+                            <tr>
+                                <th onClick={handleSortTitleToggle}>Tittel</th>
+                                <th onClick={handleSortYearToggle}>År</th>
+                                <th onClick={handleSortPageToggle}>Side</th>
+                                <th>Forfatter</th>
+                                <th>Melodi</th>
+                                <th>Arrangement</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredSongs.map((song) => (
+                                <tr key={song.id}>
+                                    <td>
+                                        <Link to={`/songs/${song.id}`}>
+                                            {song.title}
+                                        </Link>
+                                    </td>
+                                    <td>{song.year}</td>
+                                    <td>{song.start_page}</td>
+                                    <td>{song.author}</td>
+                                    <td>{song.melody}</td>
+                                    <td>{song.event}</td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {filteredSongs.map((song) => (
-                                    <tr key={song.id}>
-                                        <td>
-                                            <Link to={`/songs/${song.id}`}>
-                                                {song.title}
-                                            </Link>
-                                        </td>
-                                        <td>{song.year}</td>
-                                        <td>{song.start_page}</td>
-                                        <td>{song.author}</td>
-                                        <td>{song.melody}</td>
-                                        <td>{song.event}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </section>
-                ) : (
-                    <p className="no-matches">Ingen treff</p>
-                )
-
-                // <ul>
-                //   {filteredSongs.map((song) => (
-                //     <li key={song.id}>
-                //       <Link to={`/songs/${song.id}`}>{song.title}</Link>
-                //     </li>
-                //   ))}
-                // </ul>
-            }
+                            ))}
+                        </tbody>
+                    </table>
+                </section>
+            ) : (
+                <p className="no-matches">Ingen treff</p>
+            )}
             <Footer />
         </div>
     );
